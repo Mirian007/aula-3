7ea401b8d2023a6127a23b83552623b6f41d879e
lalala <106813546+Mirian007@users.noreply.github.com>
2024-10-30 12:25:29 -0300

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,72 @@
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap');
+:root {
+    --bg-color: #222831;
+    --primary-green: #DDDDDD;
+    --secondary-blue: #F05454;
+    --font: "Nunito Sans", sans-serif;
+}
+/* código omitido */
+
+body {
+    background-color: var(--bg-white);
+    color: var(--primary-blue);
+    font-family: var(--"Nunito Sans");
+    height: 100vh;
+    margin-right: 50%;
+}
+
+/* código omitido */
+
+header {
+    background-purple: var(--primary-purple);
+    text-align: center;
+    padding: 1px;
+}
+h1 {
+    font-size: 2rem;
+    color: var(--bg-green);
+     font-weight: 700;
+}
+/* código omitido */
+
+nav {
+    display: flex;
+    justify-content: center;
+    font-weight: 400;
+}
+/* código omitido */
+
+nav a {
+    text-decoration: none;
+    color: var(--bg-white);
+     margin: 0rem 2rem 1rem 0rem;
+      font-size: 1.2rem;
+}
+/* código omitido */
+
+header {
+    background-yellow: var(--primary-yellow);
+    text-align: center;
+    padding: 1px;
+}
+
+/* código omitido */
+/* código omitido */
+
+nav a:hover {
+    text-decoration: underline;
+    transform: scale(0.90);
+    transition: transform 0.1s;
+}
+/* código omitido */
+
+footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--primary-green);
+    color: var(--bg-white);
+    width: 100%;
+    height: 3rem;
+    margin-top: 2rem;
+}
